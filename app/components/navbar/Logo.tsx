1f209a28c2cdfe2ac84db5655c4a471eb816462f
Abhijeet Kumar <akc972527@gmail.com>
2023-05-25 06:36:43 +0530

    navbar

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 const Logo = () => {
+    const router = useRouter();
     return (
-        <div>Logo</div>
+        <Image src="/images/logo.png" alt="Logo" className="hidden cursor-pointer md:flex" height="100" width="100" />
     )
 }
 

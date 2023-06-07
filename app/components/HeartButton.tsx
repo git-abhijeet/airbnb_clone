d787cf06f8752e172e98db0c118d4aec9649ea4d
Abhijeet Kumar <akc972527@gmail.com>
2023-06-08 00:45:31 +0530

    implemented --> add to favorite functionality

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,15 +2,15 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
-interface HeartButtonProps{
+interface HeartButtonProps {
     listingId: string;
     currentUser?: SafeUser | null;
 }
 
-const HeartButton: React.FC<HeartButtonProps> = ({listingId, currentUser}) => {
-    const hasFavorited = false;
-    const toggleFavorite = () => {};
+const HeartButton: React.FC<HeartButtonProps> = ({ listingId, currentUser }) => {
+    const { hasFavorited, toggleFavorite } = useFavorite({ listingId, currentUser });
     return (
         <div onClick={toggleFavorite} className="relative transition cursor-pointer hover:opacity-80">
             <AiOutlineHeart size={28} className="fill-white absolute -top-[2px] -right-[2px]" />
